c4d1d2ac16ee36c8885c817bad1a9a54e757eaa2
John Pelletier <john.pelletier@ymail.com>
2019-05-20 12:27:52 -1000

    small update to plotting

--- errAlarm.py
+++ errAlarm.py
@@ -5,27 +5,33 @@ import select
 import matplotlib.pyplot as plt
 import csv
 
+el = ktl.cache("dcs", "el")
+az = ktl.cache("dcs", "az")
 eerr = ktl.cache("dcs", "elpe")
 aerr = ktl.cache("dcs", "azpe")
 telmove = ktl.cache("dcs", "movetel")
+el.monitor()
+az.monitor()
 eerr.monitor()
 aerr.monitor()
 telmove.monitor()
 
-x = []
-y = []
-z = []
+ee = []
+ae = []
+e = []
+a = []
+t = []
 
 trackOutFile = open('trackingout.txt', 'w+')
 ts = time.time()
 
 #write to file function
-def trackOutput(ee, ae, wind):
+def trackOutput(ee, ae, e, a, wind):
     etime = time.time() - ts
     if wind == True:
-        trackOutFile.write('%f, %f, %s, Windshake\n' % (ee, ae, etime))
+        trackOutFile.write('%f, %f, %f, %f, %s, Windshake\n' % (ee, ae, e, a, etime))
     else:
-        trackOutFile.write('%f, %f, %s\n' % (ee, ae, etime))
+        trackOutFile.write('%f, %f, %f, %f, %s\n' % (ee, ae, e, a, etime))
 
 #plotting function
 def trackPlot():
@@ -33,28 +39,52 @@ def trackPlot():
     with infile as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
         for row in plots:
-            x.append(float(row[2]))
-            y.append(float(row[0]))
-            z.append(float(row[1]))
+            ee.append(float(row[0]))
+            ae.append(float(row[1]))
+            e.append(float(row[2]))
+            a.append(float(row[3]))
+            t.append(float(row[4]))
 
-    plt.subplot(2, 1, 1)
-    plt.plot(x, y, '.')
+    fig, ax1 = plt.subplots()
 
-    plt.ylabel('Errors')
-    plt.title('El Errors')
+    color = 'red'
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('El Errors', color=color)
+    ax1.plot(t, ee, '.', color = color)
+    ax1.tick_params(axis='y', labelcolor=color)
 
-    plt.subplot(2, 1, 2)
-    plt.plot(x, z, '.')
+    ax2 = ax1.twinx()
 
-    plt.xlabel('Time')
-    plt.ylabel('Errors')
-    plt.title('Az Errors')
+    color = 'blue'
+    ax2.set_ylabel('Elevation', color=color)
+    ax2.plot(t, e, '.', color = color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+#    plt.show()
+
+    fig, ax1 = plt.subplots()
+    
+    color = 'red'
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Az Errors', color=color)
+    ax1.plot(t, ae, '.', color = color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()
+    
+    color = 'blue'
+    ax2.set_ylabel('Azimuth', color=color)
+    ax2.plot(t, a, '.', color = color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
 
     plt.show()
+
 
 #main function
 try:
     while True:
+        print("monitoring")
         wind = False
         response = '(Type w and enter to mark as windshake)\a'
         while(abs(eerr) > 0.5 or abs(aerr) > 0.5):
@@ -63,7 +93,7 @@ try:
                 time.sleep(2)
             else:
                 print("\n%f, %f" % (eerr, aerr))
-                trackOutput(eerr, aerr, wind)
+                trackOutput(eerr, aerr, el, az, wind)
                 print("Tracking errors are high! %s" % response)
                 sys.stdout.write('> ')
                 sys.stdout.flush()
